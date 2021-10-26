b5fecc984a3b2e2e4c0ec0d1ac9527a575e9232c
VladimirSafronov <safron5000@mail.ru>
2021-10-26 11:05:34 +0300

    Do without therd number

--- ChangeNum/Program.cs
+++ ChangeNum/Program.cs
@@ -1,11 +1,10 @@
 ﻿// Программа смены значений переменных
 int a = 321;
 int b = 123;
-int c = 1;
 
-c = a;
-a = b;
-b = c;
+a = a + b;
+b = a - b;
+a = a - b;
 
 Console.WriteLine($"a = {a} \nb = {b}");
 Console.WriteLine("a = " + a + " b = " + b);
