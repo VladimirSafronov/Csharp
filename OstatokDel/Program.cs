5fb570952a728e1c5fdd7a5376b859f7ead0ee24
VladimirSafronov <safron5000@mail.ru>
2021-10-26 15:12:45 +0300

    Остаток деления

---
+++ OstatokDel/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа выводящая числа, которые делятся на 4 и на 10
+int current = 2;
+int finish = 128;
+int a = 4;
+int b = 10;
+
+while(current <= finish)
+{
+    if(current % a == 0 && current % b == 0)
+    Console.WriteLine(current);
+    current = current + 1;
+}
