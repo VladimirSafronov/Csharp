0f9c327c44f3b1f8744ec1c2b3bad3b4fa4fe8f9
VladimirSafronov <safron5000@mail.ru>
2021-10-26 13:14:57 +0300

    IfElse operator

---
+++ IfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, выводящая на экран максимальное число
+
+int a = new Random().Next(1, 100);
+int b = new Random().Next(1, 100);  // Говорим программе генерировать числа
+Console.WriteLine($"a = {a} b = {b}");  // Говорим показывать сгенерированные числа
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+
+else
+{
+    Console.WriteLine(b);
+}
