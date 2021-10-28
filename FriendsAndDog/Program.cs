5eaacddb45196c027bd8796c4ce8d59e15243dee
VladimirSafronov <safron5000@mail.ru>
2021-10-28 11:40:34 +0300

    Friends and dog program

---
+++ FriendsAndDog/Program.cs
@@ -0,0 +1,27 @@
+﻿// подсчет какое количество раз пробежит собака
+int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int finish = 10;
+int time = 0;
+
+while(distance > finish)
+{
+    if(friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+
+Console.WriteLine(count);
