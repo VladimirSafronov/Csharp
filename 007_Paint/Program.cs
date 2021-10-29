050a7c45db115cf526db16d624c335bcae3d8c9c
VladimirSafronov <safron5000@mail.ru>
2021-10-29 22:55:42 +0300

    Paint

---
+++ 007_Paint/Program.cs
@@ -0,0 +1,19 @@
+﻿// программа, рисующая фигуры в терминале
+void DrawRectangle(int posX, int posY, int width, int height)
+{
+    int row = 0;
+    while(row < height)
+    {
+        Console.SetCursorPosition(posX, posY + row);
+        int count = 0;
+        while(count < width)
+        {
+            Console.Write("+");
+            count++;
+        }
+        row++;
+    }
+
+}
+
+DrawRectangle(5, 2, 22, 3);
