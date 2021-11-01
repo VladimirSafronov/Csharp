3a389b2857c0d3bd70ea4df787652ab2835cf38d
VladimirSafronov <safron5000@mail.ru>
2021-11-01 16:33:55 +0300

    Factor

---
+++ Factor/Program.cs
@@ -0,0 +1,20 @@
+﻿// Простые множители 13195: 5, 7, 13 и 29
+//Какой самый большой простой делитель числа 600851475143?
+
+long number = 600851475143; 
+int factor = 2; 
+int stopNum = 1;
+while(number != stopNum)
+{
+    if(number % factor == 0)
+    {
+        number = number / factor;
+    }
+    else
+    {
+        factor++;
+    }
+}
+
+
+Console.WriteLine($"самый большой простой делитель = {factor}");
