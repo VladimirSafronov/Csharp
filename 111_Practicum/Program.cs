67d25a8d090073b2258497701199525df1de761b
VladimirSafronov <safron5000@mail.ru>
2021-11-03 22:53:03 +0300

    Practicum 001

--- 111_Practicum/Program.cs
+++ 111_Practicum/Program.cs
@@ -4,21 +4,21 @@
 // - элементы, больше 8
 // - знакочередования
 
-int number = 10;
-int [] A = new int [number];
-int index = 0;
-while(index < number)
-{
-    A [index] = new Random().Next(-50, 50);
-    index++;
-}
+// int number = 10;
+// int [] A = new int [number];
+// int index = 0;
+// while(index < number)
+// {
+//     A [index] = new Random().Next(-50, 50);
+//     index++;
+// }
 
-index = 0;
-while(index < number)
-{
-    Console.WriteLine(A[index]);
-    index++;
-}
+// index = 0;
+// while(index < number)
+// {
+//     Console.WriteLine(A[index]);
+//     index++;
+// }
 //===========создает массив В, в порядке возрастания чисел=========================
 // index = 1;
 // Console.WriteLine();
@@ -48,25 +48,133 @@ while(index < number)
 // }
 //=======создает массив В со знакочередованием===================
 
-index = 0;
-Console.WriteLine();
-Console.WriteLine(A[index]);
-while(index < number - 1)
+// index = 0;
+// Console.WriteLine();
+// Console.WriteLine(A[index]);
+// while(index < number - 1)
+// {
+//     if(A[index] >= 0)
+//     {
+//         index++;
+//         if(A[index] <= 0)
+//         {
+//             Console.WriteLine(A[index]);
+//         }
+//     }
+//     else
+//     {
+//         index++;
+//         if(A[index] >= 0)
+//         {
+//             Console.WriteLine(A[index]);
+//         }
+//     }
+// }
+
+//=======А ТЕПЕРЬ ПРИ ПОМОЩИ МЕТЕДОВ===========
+
+void CreateArray(int [] array)
 {
-    if(A[index] >= 0)
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
     {
+        array[index] = new Random().Next(-49, 50);
         index++;
-        if(A[index] <= 0)
-        {
-            Console.WriteLine(A[index]);
-        }
+    }
+}
+
+void PrintArray(int [] arr)
+{
+    int dist = arr.Length;
+    int ind = 0;
+    while(ind < dist)
+    {
+        Console.WriteLine(arr[ind]);
+        ind ++;
+    }
+}
+
+int [] arrayA = new int [10];
+
+CreateArray(arrayA);
+PrintArray(arrayA);
+Console.WriteLine();
+
+//=====================================
+// void CreateArrayB(int [] increase)
+// {
+//     int maxEl = arrayA[0];
+//     Console.WriteLine(maxEl);
+//     int count = 1;
+//     int way = arrayA.Length;
+//     while(count < way)
+//     {
+//         if(arrayA[count] > maxEl)
+//         {
+//             maxEl = arrayA[count];
+//             Console.WriteLine(maxEl);
+//         }
+//         count++;
+//     }
+// }
+// int [] arrayB = new int [10];
+// CreateArrayB(arrayB);
+// PrintArray(arrayB);
+//======================================
+
+// int [] arrayB = new int [10];
+// void CreateArrB(int [] critical)
+// {
+//     int criticalNum = 8;
+//     int count = 0;
+//     int countB = 0;
+//     int road = arrayA.Length;
+//     while(count < road)
+//     {
+//         if(arrayA[count] < criticalNum)
+//         {
+//             arrayB[countB] = arrayA[count];
+//             countB++;
+//         }
+//         count++;
+//     }
+// }
+// CreateArrB(arrayB);
+// PrintArray(arrayB);
+
+//========================================
+
+bool IsCurrentNumberPositive(int currentNumber)
+{
+    return currentNumber >= 0;
+}
+
+int [] arrayB = new int [arrayA.Length];
+
+int countA = 0;
+int countB = 0;
+bool isNextNumberPositive;
+isNextNumberPositive = !IsCurrentNumberPositive(arrayA[countA]);
+arrayB[countB] = arrayA[countA];
+countA++;
+countB++;
+while(countA < arrayA.Length)
+{
+    if(isNextNumberPositive == IsCurrentNumberPositive(arrayA[countA]))
+    {
+        arrayB[countB] = arrayA[countA];
+        countA++;
+        countB++;
+        isNextNumberPositive = !isNextNumberPositive;
     }
     else
     {
-        index++;
-        if(A[index] >= 0)
-        {
-            Console.WriteLine(A[index]);
-        }
+        countA++;
     }
+
 }
+
+PrintArray(arrayB);
+
+// последняя задача была написана при помощи друга. Идея такая была, но точно бы не смог сам ее реализовать.
