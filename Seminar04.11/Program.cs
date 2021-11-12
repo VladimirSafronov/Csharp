339ee624c2b6f780cadd6229213daab6c041d112
VladimirSafronov <safron5000@mail.ru>
2021-11-12 12:00:21 +0300

    40 exercise done

--- Seminar04.11/Program.cs
+++ Seminar04.11/Program.cs
@@ -714,6 +714,39 @@
 
 // 40. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
 
+// double[] makeArray()
+// {
+//     double[] array = new double[10];
+//     for(int i = 0; i < array.Length; i++) array[i] = new Random().NextDouble();
+//     return array;
+// }
+
+// double[] arrayA = makeArray();
+
+
+// double FindMaxValue(double[] array)
+// {
+//     double maxValue = 0.0;
+//     for(int i = 0; i < array.Length; i++) if(array[i] > maxValue) maxValue = array[i];
+//     return maxValue;
+// }
+
+
+// double FindMinValue(double[] array)
+// {
+//     double minValue = 1.0;
+//     for(int k = 0; k < array.Length; k++) if(array[k] < minValue) minValue = array[k];
+//     return minValue;
+// }
+
+
+// double SubMaxValueAndMinValue(double argMax, double argMin)
+// {
+//     return argMax - argMin;
+// }
+
+// Console.WriteLine(SubMaxValueAndMinValue(FindMaxValue(arrayA), FindMinValue(arrayA)));
+
 // Почувствуй себя лидом
 // 41. Выяснить являются ли три числа сторонами треугольника 
 // 42. Определить сколько чисел больше 0 введено с клавиатуры
