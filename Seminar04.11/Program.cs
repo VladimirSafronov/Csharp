9e65c962b27936cb018a9207f3943e9dfb67976a
VladimirSafronov <safron5000@mail.ru>
2021-11-16 22:30:55 +0300

    42, 43 exercises done

--- Seminar04.11/Program.cs
+++ Seminar04.11/Program.cs
@@ -743,7 +743,7 @@
 
 // Console.WriteLine(IsItTriangle(a, b, c));
 
-// 42. Определить сколько чисел больше 0 введено с клавиатуры
+// // 42. Определить сколько чисел больше 0 введено с клавиатуры
 
 // int EnteredNumbers(int enter)
 // {
@@ -760,6 +760,70 @@
 // Console.WriteLine(res);
 
 // 43. Написать программу преобразования десятичного числа в двоичное
+
+// int[] createArr(int arg)
+// {
+//     int[] array = new int[10];
+//     int i = 0;
+//     while(arg > 0)
+//     {
+//         if(arg % 2 == 0) array[i] = 0;
+//         else array[i] = 1;
+//         i++;
+//         arg = arg / 2;
+//     }
+//     return array;
+// }
+
+// int[] ChangeToBinary(int[] array)
+// {
+//     int last = array.Length -1; int temporary = 0;
+//     for(int first = 0; first < array.Length / 2; first++)
+//     {
+//         temporary = array[first]; 
+//         array[first] = array[last];
+//         array[last] = temporary;
+//         last --;
+//     }
+//     return array;
+// }
+
+// int[] firstArr = ChangeToBinary(createArr(657));
+// Console.WriteLine(String.Join(' ', firstArr));
+
+//_________________________
+
+// int decimalNum = 765646362;
+
+// int findIndexNumArray(int arg)
+// {
+//     int i = 0; 
+//     while(arg > 0)
+//     {
+//         arg = arg/2;
+//         i++;
+//     }
+//     return i;
+// }
+
+// int[] ChangeToBinary(int index, int arg)
+// {
+//     int[] array = new int[index];
+//     int last = array.Length - 1;
+//     for(int j = 0; j < array.Length; j++)
+//     {
+//         if(arg % 2 == 0) array[last] = 0;
+//         else array[last] = 1;
+//         arg = arg / 2;
+//         last -=1;
+//     }
+//     return array;
+// }
+
+
+// int[] binarArray = ChangeToBinary(findIndexNumArray(decimalNum), decimalNum);
+// Console.WriteLine(String.Join(' ', binarArray));
+
 // 44. Найти точку пересечения двух прямых заданных уравнением y=kx+b, а1 k1 и а2 и k2 заданы
 // 45. Показать числа Фибоначчи
 // 46. Написать программу масштабирования фигуры
