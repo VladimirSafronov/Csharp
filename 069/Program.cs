834516de0984a7a8a5417789641d0007df0d0231
VladimirSafronov <safron5000@mail.ru>
2021-12-02 15:28:01 +0600

    69 exercise done

---
+++ 069/Program.cs
@@ -0,0 +1,9 @@
+﻿//69. Показать натуральные числа от M до N, N и M заданы
+
+string ShowNumbers(int m, int n)
+{
+    if(m == n) return  $" {m}";
+    return $" {m + ShowNumbers(m+1, n)}";
+}
+
+Console.WriteLine(ShowNumbers(-4, 5));
