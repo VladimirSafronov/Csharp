06907edfaa0f86dd6e48d06e7d52800f5c144562
VladimirSafronov <safron5000@mail.ru>
2021-12-02 21:37:54 +0600

    68 done

---
+++ 068/Program.cs
@@ -0,0 +1,9 @@
+﻿// Это 69 задача. Найти сумму элементов от M до N, N и M заданы
+
+int Sum(int m, int n)
+{
+    if(m == n) return m;
+    return m + Sum(m + 1, n);
+}
+
+Console.WriteLine(Sum(1, 4));
