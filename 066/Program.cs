fea88643656969cff8d44d276f72a2c2808c72c0
VladimirSafronov <safron5000@mail.ru>
2021-12-02 15:50:35 +0600

    66 exercise done

---
+++ 066/Program.cs
@@ -0,0 +1,9 @@
+﻿// 66. Показать натуральные числа от 1 до N, N задано
+
+string ShowNumbers(int n)
+{
+    if(n == 1) return $" {1}";
+    return $" {n + ShowNumbers(n - 1)}";
+}
+
+Console.WriteLine(ShowNumbers(10));
