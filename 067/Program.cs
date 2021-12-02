5c373e63a4a98681b9a386af7831413020f59792
VladimirSafronov <safron5000@mail.ru>
2021-12-02 21:25:26 +0600

    67 done

---
+++ 067/Program.cs
@@ -0,0 +1,9 @@
+﻿// 67. Показать натуральные числа от N до 1, N задано
+
+string ShowNumbers(int n)
+{
+    if(n == 1) return $" {n}";
+    return $" {n + ShowNumbers(n - 1)}";
+}
+
+Console.WriteLine(ShowNumbers(7));
