613d6880a14cd55dbcb76a04616835f700193383
VladimirSafronov <safron5000@mail.ru>
2021-12-03 18:58:59 +0600

    49 done

---
+++ 049/Program.cs
@@ -0,0 +1,23 @@
+﻿// 49. Показать двумерный массив размером m×n заполненный вещественными числами
+
+double[,] FillArray(int m, int n)
+{
+    double[,] array = new double[m, n];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++) array[i, j] = new Random().NextDouble();
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++) Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+double[,] matrix = FillArray(4, 4);
+PrintArray(matrix);
