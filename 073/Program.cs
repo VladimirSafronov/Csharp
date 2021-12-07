bc52a29c21f6c0d7bb74cf61c4c5f86c1006a21f
VladimirSafronov <safron5000@mail.ru>
2021-12-07 13:36:19 +0600

    73 done

---
+++ 073/Program.cs
@@ -0,0 +1,10 @@
+﻿// 73. Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+//Первые два элемента последовательности задаются пользователем.
+
+string FirstTwoNumbersSum(int a, int b, int n)
+{
+    if (a > n) return "";
+    else return $" {a + FirstTwoNumbersSum(b, b + a, n)}";
+}
+
+Console.WriteLine(FirstTwoNumbersSum(1, 2, 160));
