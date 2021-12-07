7c79914c82180a2bd49fd225feafae8472c74e00
VladimirSafronov <safron5000@mail.ru>
2021-12-07 07:41:36 +0600

    72 done

---
+++ 072/Program.cs
@@ -0,0 +1,9 @@
+﻿// 72. Написать программу возведения числа А в целую стень B
+
+int Degree(int a, int b)
+{
+    if(b == 0) return 1;
+    return a * Degree(a, b - 1);
+}
+
+Console.WriteLine(Degree(3, 3));
