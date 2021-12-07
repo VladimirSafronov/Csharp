10f135e9b6a07e55b9f1c86e1b4b7bf96aa1f92e
VladimirSafronov <safron5000@mail.ru>
2021-12-07 14:51:28 +0600

    Sergey's decision 74 exercise

---
+++ 074/Program.cs
@@ -0,0 +1,17 @@
+﻿// 74. В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». Покажите все слова, состоящие из n букв, которые 
+// можно построить из букв этого алфавита.
+int k = 1;
+void ShowWords(string text, char[] letter, int length = 0)
+{
+    if (length == letter.Length)
+    {
+        Console.WriteLine($"{k++} {new String(letter)}"); return;
+    }
+    for (int i = 0; i < text.Length; i++)
+    {
+        letter[length] = text[i];
+        ShowWords(text, letter, length +1);
+    }
+}
+
+ShowWords("аисв", new char[4]);
