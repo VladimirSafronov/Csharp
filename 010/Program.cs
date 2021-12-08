516defe59b2bb3b24e69c4560993d5add9f45f98
VladimirSafronov <safron5000@mail.ru>
2021-12-08 20:19:22 +0600

    10 done

---
+++ 010/Program.cs
@@ -0,0 +1,9 @@
+﻿// 10. =========Показать вторую цифру трёхзначного числа========
+
+int SecNumber(int arg)
+{
+    if(arg < 0) arg = arg * -1;
+    return arg / 10 % 10;
+}
+
+Console.WriteLine(SecNumber(735));
