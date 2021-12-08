0ba0be0999224937be42c70b36a15ffbf49a3174
VladimirSafronov <safron5000@mail.ru>
2021-12-08 20:32:48 +0600

    013 done

---
+++ 012/Program.cs
@@ -0,0 +1,9 @@
+﻿// 12. ======Удалить вторую цифру трёхзначного числа=========
+
+int DeleteSecNumber(int arg)
+{
+    int a = arg / 100;
+    int b = arg % 10;
+    return a * 10 + b;
+}
+Console.WriteLine(DeleteSecNumber(-341));
---
+++ 013/Program.cs
@@ -0,0 +1,24 @@
+﻿// 13. =======Выяснить, кратно ли число заданному, если нет, вывести остаток.=======
+
+int CreateNum()
+{
+    return new Random().Next(10, 100);
+}
+
+int CreateDev()
+{
+    return new Random().Next(2, 10);
+}
+
+int firstNum = CreateNum();
+int secondNum = CreateDev();
+Console.Write($"{firstNum} {secondNum}");
+Console.WriteLine();
+
+int Multiple(int a, int b)
+{
+    if(a % b == 0) return -1;   //в случае кратности чисел выведет -1
+    else return firstNum % secondNum;
+}
+
+Console.WriteLine(Multiple(firstNum, secondNum));
