587814919cc5ddeebed70950717e3025e49c5555
VladimirSafronov <safron5000@mail.ru>
2021-12-08 19:04:15 +0600

    004 done

---
+++ 004/Program.cs
@@ -0,0 +1,23 @@
+﻿//  4. Найти максимальное из трех чисел
+
+int FindMaxNumber(int x, int y, int z)
+{
+    int max = x;
+    if(y > max) max = y;
+    if(z > max) max = z;
+    return max;
+}
+
+int CreateNum()
+{
+    return new Random().Next(1, 100);
+}
+
+int a = CreateNum();
+Console.Write($"{a} ");
+int b = CreateNum();
+Console.Write($"{b} ");
+int c = CreateNum();
+Console.WriteLine(c);
+
+Console.WriteLine($"Максимальное число {FindMaxNumber(a, b, c)}");
