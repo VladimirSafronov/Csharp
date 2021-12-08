5ac7be4f47b9488eccace4d4dcc724ec0e46caef
VladimirSafronov <safron5000@mail.ru>
2021-12-08 19:02:24 +0600

    003 done

---
+++ 003/Program.cs
@@ -0,0 +1,8 @@
+﻿//  3. =======По заданному номеру дня недели вывести его название=======
+
+string Func(int x)
+{
+    string [] array = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    return array[x - 1]; 
+}
+Console.WriteLine(Func(4));
