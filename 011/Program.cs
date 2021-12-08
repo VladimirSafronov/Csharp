d2bd014c44e16f48bb5bc20c37e33f18c20e5bb1
VladimirSafronov <safron5000@mail.ru>
2021-12-08 20:28:47 +0600

    11 done

---
+++ 011/Program.cs
@@ -0,0 +1,18 @@
+﻿// 11. =========Дано число из отрезка [10, 99]. Показать наибольшую цифру числа=======
+
+int MaxNum(int arg)
+{
+    int a = arg / 10;
+    int b = arg % 10;
+    if(a > b) return a;
+    else return b;
+}
+
+int CreateNum()
+{
+    return new Random().Next(10, 100);
+}
+
+int number = CreateNum();
+Console.WriteLine(number);
+Console.WriteLine(MaxNum(number));
