2082b7bbc5f2b1528cb8d4cd54db4b6ad0e59e61
VladimirSafronov <safron5000@mail.ru>
2021-12-08 20:20:37 +0600

    009 done

---
+++ 009/Program.cs
@@ -0,0 +1,8 @@
+﻿//  9. =====Показать последнюю цифру трёхзначного числа======
+
+int LastNumber(int arg)
+{
+    if(arg < 0) arg = arg * -1;
+    return arg % 10;
+}
+Console.WriteLine(LastNumber(349));
