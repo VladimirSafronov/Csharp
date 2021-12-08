bfcea7126181849fd3c46af091a7f70e821307b8
VladimirSafronov <safron5000@mail.ru>
2021-12-08 20:36:52 +0600

    015 done

---
+++ 015/Program.cs
@@ -0,0 +1,9 @@
+﻿// 15. =========Дано число. Проверить кратно ли оно 7 и 23
+
+bool Number(int num)
+{
+    if(num % 7 == 0 && num % 23 == 0) return true;
+    else return false;
+}
+
+Console.WriteLine(Number(161));
