f9c8a0b46b1c614c3f5bb6179cd9c07053900db3
VladimirSafronov <safron5000@mail.ru>
2021-12-09 22:41:29 +0600

    017 done

---
+++ 017/Program.cs
@@ -0,0 +1,9 @@
+﻿// 17. =====По двум заданным числам проверять является ли одно квадратом другого
+
+bool Func17(int a, int b)
+{
+    if (a == b * b || b == a * a ) return true;
+    else return false;
+}
+
+Console.WriteLine(Func17(26, 5));
