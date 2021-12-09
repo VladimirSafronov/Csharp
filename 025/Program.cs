230c4b56393e95ee7e34d8b8ed07bf3fd42d2a61
VladimirSafronov <safron5000@mail.ru>
2021-12-09 23:03:23 +0600

    025 done

---
+++ 025/Program.cs
@@ -0,0 +1,21 @@
+﻿// 25. Найти сумму чисел от 1 до А
+
+double a = 1000;
+double SumNumbersFrom1(double arg)
+{
+    double result = 0;
+    for(double i = 1; i <= arg; i++) result = result + i;
+    return result;
+}
+double res = SumNumbersFrom1(a);
+DateTime dt = DateTime.Now;
+Console.WriteLine(res);
+Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+// //__________________________
+// int SumNumbers2(int arg)
+// {
+//     return (arg * (arg + 1)) / 2;  //формула, сильно ускоряющая работу метода
+// }
+// dt = DateTime.Now;
+// Console.WriteLine(SumNumbers2(a));
+// Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
