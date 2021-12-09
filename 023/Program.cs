4ca738162d5e1ab4b9243a073617e6a425b0deb1
VladimirSafronov <safron5000@mail.ru>
2021-12-09 22:55:12 +0600

    023 done

---
+++ 023/Program.cs
@@ -0,0 +1,12 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N 
+
+double[] FuncPow(int arg)
+{
+    double [] array = new double [arg];
+    for(int i = 1; i <= arg; i++) array[i-1] = Math.Pow(i, 2);
+    return array;
+}
+int n = 10;
+double[] Array = FuncPow(n);
+
+for(int k = 1; k <= Array.Length; k++) Console.WriteLine($"{k}^2 = {Array[k-1]}");
