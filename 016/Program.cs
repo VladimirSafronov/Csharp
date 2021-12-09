e13d6272a20ed61fdfc6ca4e174ac42580743a4a
VladimirSafronov <safron5000@mail.ru>
2021-12-09 22:40:01 +0600

    016 done

---
+++ 016/Program.cs
@@ -0,0 +1,9 @@
+﻿// 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным днём
+
+bool FindWeekend(int arg)
+{
+    if (arg == 6 || arg == 7) return true;
+    else return false;
+}
+
+Console.WriteLine(FindWeekend(7));
