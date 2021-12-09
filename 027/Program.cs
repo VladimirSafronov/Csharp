9ccc3ac5f9e41e6c5340c40c5d5cb8b92ed75e02
VladimirSafronov <safron5000@mail.ru>
2021-12-09 23:09:13 +0600

    027 done

---
+++ 027/Program.cs
@@ -0,0 +1,15 @@
+﻿// 27. Определить количество цифр в числе
+
+int FindQuantity(int arg)
+{
+    arg = Math.Abs(arg);
+    int result = 1;
+    while(arg > 10) 
+    {
+        arg = arg / 10;
+        result ++;
+    }
+    return result;
+}
+
+Console.WriteLine(FindQuantity(-8641219));
