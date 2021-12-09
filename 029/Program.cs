8f1a4d3d4791655263c6a80fdf91bfc10757dc49
VladimirSafronov <safron5000@mail.ru>
2021-12-09 23:12:13 +0600

    029 done

---
+++ 029/Program.cs
@@ -0,0 +1,10 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+int n = 10;
+int MultiplicationNumFrom1(int arg)
+{
+    int result = 1;
+    for(int i = 1; i <= arg; i++) result = result * i;
+    return result;
+}
+Console.WriteLine(MultiplicationNumFrom1(n));
