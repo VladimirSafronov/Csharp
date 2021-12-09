69d66922a7b0a52da7cb98df3571a38f6be7a895
VladimirSafronov <safron5000@mail.ru>
2021-12-09 23:10:43 +0600

    028 done

---
+++ 028/Program.cs
@@ -0,0 +1,15 @@
+﻿// 28. Подсчитать сумму цифр в числе
+
+int SumNumbers(int num)
+{
+    int total = 0;
+    num = Math.Abs(num);
+    while(num != 0)
+    {
+        total = total + num % 10;
+        num = num / 10;
+    }
+    return total;
+}
+
+Console.WriteLine(SumNumbers(1135));
