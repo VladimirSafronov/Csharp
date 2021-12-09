72a4630911f404bd9ce598662ca564b6ad280945
VladimirSafronov <safron5000@mail.ru>
2021-12-09 22:56:57 +0600

    024 done

---
+++ 024/Program.cs
@@ -0,0 +1,15 @@
+﻿// 24. Найти кубы чисел от 1 до N
+
+double[] FuncPow(int n)
+{
+    double [] array = new double [n];
+    for(int i = 1; i <= n; i++)
+    {
+        array[i - 1] = Math.Pow(i, 3);
+    }
+    return array;
+}
+
+double [] Cube = FuncPow(10);
+
+Console.WriteLine(String.Join(',', Cube));
