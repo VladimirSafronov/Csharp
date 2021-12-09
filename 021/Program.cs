0708e83074826f4e48a29b6d04e0fedb5cdcde34
VladimirSafronov <safron5000@mail.ru>
2021-12-09 22:48:52 +0600

    021 done

---
+++ 021/Program.cs
@@ -0,0 +1,9 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом.
+
+bool Func21(int n)
+{
+    if(n / 10000 % 10 == n % 10 && n / 1000 % 10 == n / 10 % 10) return true;
+    else return false;
+}
+
+Console.WriteLine(Func21(48594));
