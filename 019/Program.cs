cbee0f6c5174c7973ae3018242bc824983d57ec6
VladimirSafronov <safron5000@mail.ru>
2021-12-09 22:44:32 +0600

    019 done

---
+++ 019/Program.cs
@@ -0,0 +1,11 @@
+﻿// 19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+int FindQuarter(int x, int y)
+{
+    if(x > 0 & y > 0) return 1;
+    if(x < 0 & y > 0) return 2;
+    if(x < 0 & y < 0) return 3;
+    else return 4;
+}
+
+Console.WriteLine(FindQuarter(3, -3));
