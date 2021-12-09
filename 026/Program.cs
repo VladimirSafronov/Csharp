6b36cdbb0226976acbbf4843f092e74b2a6681f0
VladimirSafronov <safron5000@mail.ru>
2021-12-09 23:07:31 +0600

    026 done

---
+++ 026/Program.cs
@@ -0,0 +1,12 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+
+double a = 1.13;
+int b = 5;
+
+double FuncPow(double argA, int argB)
+{
+    double result = argA;
+    for(int i = 1; i < argB; i++) result = result * argA;
+    return result;
+}
+Console.WriteLine(FuncPow(a, b));
