4846cb778dcdab62238e2183d1baae7d84f511bb
VladimirSafronov <safron5000@mail.ru>
2021-12-09 12:14:53 +0600

    071 done

---
+++ 071/Program.cs
@@ -0,0 +1,15 @@
+﻿// 71. Написать программу вычисления функции Аккермана
+//m == 0    (n + 1)
+//m > 0, n == 0     (A(m - 1, 1))
+//m > 0, n > 0     (A(m - 1, A(m, n - 1)))
+
+
+int AkkermanFunc(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return AkkermanFunc(m - 1, 1);
+    else return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+}
+
+Console.WriteLine(AkkermanFunc(4, 1));
+
