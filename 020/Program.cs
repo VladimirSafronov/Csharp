d066a3c7598f3eb4dedbf44b7e40693c35bd835f
VladimirSafronov <safron5000@mail.ru>
2021-12-09 22:46:47 +0600

    020 done

--- 020/Program.cs
+++ 020/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 20. Ввести номер четверти, показать диапазоны для возможных координат
+
+string InputQuarter()
+{
+    Console.WriteLine("Введите номер четверти: ");
+    string a = Console.ReadLine();
+    int arg = Convert.ToInt16(a);
+    if(arg == 1) return ($"x>0, y>0");
+    if(arg == 2) return ($"x<0, y>0");
+    if(arg == 3) return ($"x<0, y<0");
+    if(arg == 4) return ($"x>0, y<0");
+    else return ($"Впишите номер от 1 до 4!");
+}
+
+string ans = InputQuarter();
+Console.WriteLine(ans);
