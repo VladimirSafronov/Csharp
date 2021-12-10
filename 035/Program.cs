26bd87a6268e3809eb71f8d5d586664a83964a6f
VladimirSafronov <safron5000@mail.ru>
2021-12-10 16:16:05 +0600

    035 done

---
+++ 035/Program.cs
@@ -0,0 +1,28 @@
+﻿// 35. Определить, присутствует ли в заданном массиве, некоторое число 
+
+int[] MadeArray()
+{
+    int[] array = new int [10];
+    for(int i = 0; i < array.Length; i++) array[i] = new Random().Next(0, 10);
+    return array;
+}
+
+bool FindNumber(int arg, int[] array)
+{
+    bool ans = false;
+    int k = 0;
+    while(k < array.Length) 
+    {
+        if(array[k] == arg) 
+        {
+            ans = true;
+            break;
+        }
+        k++;
+    }
+    return ans;
+}
+
+int[] NewArray = MadeArray();
+Console.WriteLine(String.Join(',', NewArray));
+Console.WriteLine(FindNumber(5, NewArray));
