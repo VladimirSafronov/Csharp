d2c694c4df003e5e0d646ab08b3c524f4b3661b6
VladimirSafronov <safron5000@mail.ru>
2021-12-10 18:16:12 +0600

    040 done

---
+++ 040/Program.cs
@@ -0,0 +1,30 @@
+﻿// 40. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+double[] makeArray()
+{
+    double[] array = new double[10];
+    for(int i = 0; i < array.Length; i++) array[i] = new Random().NextDouble();
+    return array;
+}
+
+double FindMaxValue(double[] array)
+{
+    double maxValue = 0.0;
+    for(int i = 0; i < array.Length; i++) if(array[i] > maxValue) maxValue = array[i];
+    return maxValue;
+}
+
+double FindMinValue(double[] array)
+{
+    double minValue = 1.0;
+    for(int k = 0; k < array.Length; k++) if(array[k] < minValue) minValue = array[k];
+    return minValue;
+}
+
+double SubMaxValueAndMinValue(double argMax, double argMin)
+{
+    return argMax - argMin;
+}
+
+double[] arrayA = makeArray();
+Console.WriteLine(SubMaxValueAndMinValue(FindMaxValue(arrayA), FindMinValue(arrayA)));
