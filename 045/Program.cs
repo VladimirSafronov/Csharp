a296946f1899d65e3eabe0711ffc0c3d8575d905
VladimirSafronov <safron5000@mail.ru>
2021-12-10 22:05:28 +0600

    045 done

---
+++ 045/Program.cs
@@ -0,0 +1,9 @@
+﻿// 45. Показать числа Фибоначчи
+
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for(int i = 1; i < 11; i++) Console.WriteLine(Fibonacci(i));
