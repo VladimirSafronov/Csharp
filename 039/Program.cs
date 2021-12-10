c474ca24d75bd27a74476405a4f152e628a46d84
VladimirSafronov <safron5000@mail.ru>
2021-12-10 18:13:49 +0600

    039 done

---
+++ 039/Program.cs
@@ -0,0 +1,25 @@
+﻿// 39. Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+int[] CreateArray()
+{
+    int[] array = new int [10];
+    for(int i = 0; i < array.Length; i++) array[i] = new Random().Next(0, 10);
+    return array;
+}
+
+int[] multiplicationPairs(int[] array)
+{
+    int rear = array.Length -1;
+    int[] multPairs = new int[array.Length / 2];  //при нечетной длинне массива, средний элемент теряется, т.к. для него нет пары
+    for(int count = 0; count < multPairs.Length; count++) 
+    {
+        multPairs[count] = array[count] * array [rear];
+        rear--;
+    }
+    return multPairs;
+}
+
+int[] arrayA = CreateArray();
+Console.WriteLine(String.Join(',', arrayA));
+int[] arrayB = multiplicationPairs(arrayA);
+Console.WriteLine(String.Join(',', arrayB));
