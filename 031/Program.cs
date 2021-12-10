d6890d53e62673a992771fff428cf7753ac7e9a4
VladimirSafronov <safron5000@mail.ru>
2021-12-10 16:08:18 +0600

    031 done

---
+++ 031/Program.cs
@@ -0,0 +1,12 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+
+string[] week = { "Дни недели:", " пн", " вт", " ср", " чт", " пт", " сб", " вс" };
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < 8; i++) Console.Write(arr[i]);
+}
+
+PrintArray(week);
+
+Console.WriteLine(String.Join(',', week)); //упрощенный вывод массива
