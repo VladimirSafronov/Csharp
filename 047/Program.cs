7cca043ddae1132245aefb11693c6b4897b30de8
VladimirSafronov <safron5000@mail.ru>
2021-12-11 23:05:36 +0600

    047 done

---
+++ 047/Program.cs
@@ -0,0 +1,24 @@
+﻿// 47. Написать программу копирования массива
+
+int[] FillArray (int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    return array;
+}
+
+int[] Copy(int[] array)
+{
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++) newArray[i] = array[i];
+    return newArray;
+}
+
+int[] numbers = FillArray(10);
+Console.WriteLine(String.Join(',', numbers));
+Console.WriteLine();
+int[] numbersCopy = Copy(numbers);
+Console.WriteLine(String.Join(',', numbersCopy));
